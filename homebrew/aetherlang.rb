2df22773c075b32c7d6c9224f198e26e314ca03c
Virul Nirmala Wickramasinghe <89099391+iamvirul@users.noreply.github.com>
2025-05-12 23:55:22 +0530

    Implement code changes to enhance functionality and improve performance

--- homebrew/aetherlang.rb
+++ homebrew/aetherlang.rb
@@ -4,16 +4,20 @@ class Aetherlang < Formula
   version "1.1.0"
   license "MIT"
 
-  url "https://github.com/iamvirul/Aetherlang/releases/download/v1.1.0/aeth" # Direct download URL
-  sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5" # Replace with the actual SHA-256 checksum
+  url "https://github.com/iamvirul/Aetherlang/releases/download/v1.1.0/aeth"
+  sha256 "572b1cf8e9a0b9c6b8f6fe4d111ca7ee23c6e7439df4e0d216b813531a876e1b"
+
+  resource "aethc" do
+    url "https://github.com/iamvirul/Aetherlang/releases/download/v1.1.0/aethc"
+    sha256 "7e860aa6fcee54fd54be7269b2e32fc45d91541efdc5253036d9d603f68bddcd"
+  end
 
   def install
     bin.install "aeth"
-    # There is no aethc binary in the release
+    resource("aethc").stage { bin.install "aethc" }
   end
 
   test do
-    # Add a simple test here to verify that Aetherlang is working
     system "#{bin}/aeth", "--version"
   end
 end
